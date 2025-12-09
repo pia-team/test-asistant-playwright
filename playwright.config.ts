f1ac81876a603d625240b5c5c18e8720455682a8
burakkizirpiateam <burak.kizir@dnext-technology.com>
2025-12-09 16:00:38 +0300

    fix: video

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,16 +4,18 @@ export default defineConfig({
   timeout: 60000,
   // retries:2,//retry numbers
   // workers:4,//browser numbers
-   reporter: [["line"], ["allure-playwright"]],
+  reporter: [["line"], ["allure-playwright"]],
   use: {
     //headless: false,// headless modu world.ts içerisindenden yönetiliyor ya da consolde npm run ...:headless koduyla koşulabilir
 
-    viewport:null,
+    viewport: { width: 1920, height: 1080 }, // Tam ekran video için sabit viewport
     launchOptions: {
-    args: ['--start-maximized'] // ✅ Chrome/Edge için pencereyi büyüt
-  },
-    //viewport: { width: 1280, height: 720 },
+      args: ['--start-maximized'] // ✅ Chrome/Edge için pencereyi büyüt
+    },
     screenshot: 'on', // test başarısız olursa ekran görüntüsü al
-    //video: 'retain-on-failure', //video kaydı al
+    video: {
+      mode: 'on', // Her zaman video kaydı al
+      size: { width: 1920, height: 1080 } // Video boyutunu viewport ile eşleştir
+    },
   },
 });
